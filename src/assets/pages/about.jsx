44d14b051c253f6a85cc686ce967d509df312aa8
Oyyanv <muhammadroyyan527@gmail.com>
2025-09-05 03:20:07 +0800

    refactor

--- src/assets/pages/about.jsx
+++ src/assets/pages/about.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import Lanyard from '../components/Lanyard/Lanyard';
+import BlurText from '../../../Reactbit/BlurText/BlurText';
 
 const About = () => {
   return (
     <section id="about" className="relative w-screen h-screen flex items-center justify-center">
-      
+
       {/* background text */}
-      <h1 className="absolute xl:text-[14rem] xl:mb-[20%] text-[5rem] mb-[22rem] font-Lexend font-light text-center text-white/5 select-none z-0">
-        ABOUT ME
-      </h1>
+      <div className='absolute text-[14rem] mb-[25%] font-Lexend font-light text-white/5 select-none z-0'>
+        <div className='hidden xl:block'>
+          <BlurText
+            text="ABOUT ME"
+            animateBy='letters'
+            direction='top'
+            delay={100}
+            className='inline-block '
+          />
+        </div>
+
+        <div className="flex flex-col mb-[18rem] items-center xl:hidden">
+          <BlurText
+            text="ABOUT"
+            animateBy="letters"
+            direction="top"
+            delay={200}
+            className="font-Lexend font-light text-white/5 text-[5rem] select-none text-center"
+          />
+          <BlurText
+            text="ME"
+            animateBy="letters"
+            direction="top"
+            delay={1000}
+            className="font-Lexend font-light text-white/5 text-[5rem] select-none text-center"
+          />
+        </div>
+      </div>
 
       {/* foreground content */}
       <div className="h-screen w-screen relative z-10 flex flex-col items-center">
-        <Lanyard 
+        <Lanyard
           position={[0, 0, 20]}
           gravity={[0, -40, 0]}
           fov={17}
